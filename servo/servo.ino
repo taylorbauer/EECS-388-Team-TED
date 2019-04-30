17e3b4ccec6a2067ebd8d5dbd4283e074b0711ee
Taylor Bauer <taylorbauer@ku.edu>
2019-04-30 10:46:37 -0500

    Added timer library and integrated into our servo task, needs to be tested on hardware

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,19 +1,27 @@
 #include <Servo.h>
+#include <SimpleTimer.h>
 
 // Variable declarations:
 Servo myservo; //create servo object to control servo
+
+SimpleTimer timer; //Global SimpleTimer object (necessary for timer)
+
 const int buttonPin = 2; //the pin for the button
 int buttonState = 0; // the state of the button (defaults to 0)
 bool lastButtonState = LOW; // a variable that tracks the previous button state
 
 // Setup:
 void setup() {
+  timer.setInterval(3600000, timedDispense);  //first parameter is time in ms (one hour for now)
+  //second parameter is the function to call every x milliseconds
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   pinMode(buttonPin, INPUT); // set buttonpin as an input
 }
 
 // Main Loop:
 void loop() {
+
+  timer.run();
 
   buttonState = digitalRead(buttonPin);  // check the state of the button (this is so easy compared to lab!)
   if (buttonState != lastButtonState) {
@@ -32,4 +40,13 @@ void loop() {
       delay(10);
     }
   }
+}
+
+void timedDispense() {  // The function that is called every 6 hours to dispense cat food
+  myservo.write(180); 
+  delay(500);         
+  
+  myservo.write(20); 
+  delay(500);
+       
 }
